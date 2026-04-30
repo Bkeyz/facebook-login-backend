22b3686df19234aeae2620cd036ba16fb4be7776
Bkeyz <davidshofolahan@gmail.com>
2026-04-30 12:07:37 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ const TELEGRAM_CHAT_ID = '5707645216';      // ← Put your chat ID here
 // ====================================================================
 
 // Middleware
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: false,
+}));
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
